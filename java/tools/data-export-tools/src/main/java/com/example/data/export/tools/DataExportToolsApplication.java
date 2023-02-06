a957fe154f93d91c3db3a6dd96d961500727e43f
likeguo <li_keguo@163.com>
2023-02-06 22:11:11 +0800

    tools

---
+++ java/tools/data-export-tools/src/main/java/com/example/data/export/tools/DataExportToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.data.export.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExportToolsApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DataExportToolsApplication.class, args);
+    }
+    
+}
