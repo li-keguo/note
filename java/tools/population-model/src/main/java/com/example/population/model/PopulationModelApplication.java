33b8d098f3fa163bc7870ee7f2cfa6fe101bbadf
likeguo <li_keguo@163.com>
2023-02-07 23:19:20 +0800

    tools

---
+++ java/tools/population-model/src/main/java/com/example/population/model/PopulationModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.population.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulationModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulationModelApplication.class, args);
+	}
+
+}
