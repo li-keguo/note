4906451443310f62d7d65ab44feec4b0cfbc2f7d
likeguo <li_keguo@163.com>
2023-04-09 21:43:32 +0800

    fink

--- java/fink/fink-demo/src/main/java/com/example/fink/finkdemo/FinkDemoApplication.java
+++ java/fink/fink-demo/src/main/java/com/example/fink/finkdemo/FinkDemoApplication.java
@@ -1,15 +1,7 @@
 package com.example.fink.finkdemo;
 
-import lombok.Data;
-import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author keguoli
